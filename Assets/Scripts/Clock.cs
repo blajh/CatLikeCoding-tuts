3c6821c0b64206f77e78f4df58e2c9433673145e
Andrej <glazba@gmail.com>
2020-10-03 00:25:05 +0200

    Added Clock and Analog Clock

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+
+	const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+
+	[SerializeField]
+	Transform hoursPivot = default, minutesPivot = default, secondsPivot = default;
+
+	void Awake()
+	{
+		DateTime time = DateTime.Now;
+		hoursPivot.localRotation =
+			Quaternion.Euler(0f, 0f, hoursToDegrees * time.Hour);
+		minutesPivot.localRotation =
+			Quaternion.Euler(0f, 0f, minutesToDegrees * time.Minute);
+		secondsPivot.localRotation =
+			Quaternion.Euler(0f, 0f, secondsToDegrees * time.Second);
+	}
+
+    void Update () {
+		var time = DateTime.Now;
+		hoursPivot.localRotation =
+			Quaternion.Euler(0f, 0f, hoursToDegrees * time.Hour);
+		minutesPivot.localRotation =
+			Quaternion.Euler(0f, 0f, minutesToDegrees * time.Minute);
+		secondsPivot.localRotation =
+			Quaternion.Euler(0f, 0f, secondsToDegrees * time.Second);
+	}
+
+}
---
+++ Assets/Scripts/Clock1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class Clock1 : MonoBehaviour
+{
+
+	const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+
+	[SerializeField]
+	Transform hoursPivot = default, minutesPivot = default, secondsPivot = default;
+
+	void Awake()
+	{
+		DateTime time = DateTime.Now;
+		hoursPivot.localRotation =
+			Quaternion.Euler(0f, 0f, hoursToDegrees * time.Hour);
+		minutesPivot.localRotation =
+			Quaternion.Euler(0f, 0f, minutesToDegrees * time.Minute);
+		secondsPivot.localRotation =
+			Quaternion.Euler(0f, 0f, secondsToDegrees * time.Second);
+	}
+
+	void Update()
+	{
+		TimeSpan time = DateTime.Now.TimeOfDay;
+		hoursPivot.localRotation =
+			Quaternion.Euler(0f, 0f, hoursToDegrees * (float)time.TotalHours);
+		minutesPivot.localRotation =
+			Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
+		secondsPivot.localRotation =
+			Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
+	}
+
+}
