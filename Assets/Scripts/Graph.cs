a15a71dfe5b2a459a73f0927b55d0e7933427bd3
Andrej <glazba@gmail.com>
2020-10-04 00:48:26 +0200

    Second lexturee done - animated sine cubes with custom shaders

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -11,21 +11,38 @@ public class Graph : MonoBehaviour
 	[SerializeField, Range (10, 100)]
 	int resolution = 10;
 
+	Transform[] points;
+
 	private Transform point = default;
 
     void Awake()
     {
+		points = new Transform[resolution];
 		float step = 2f / resolution;
         var position = Vector3.zero;
 		var scale = Vector3.one * step;
-		for (int i = 0; i < resolution; i++)
+		for (int i = 0; i < points.Length; i++)
 		{
 			Transform point = Instantiate(pointPrefab);
 			position.x = (i + 0.5f) * step - 1f;
-			position.y = position.x * position.x * position.x;
+			//position.y = position.x * position.x * position.x;
 			point.localPosition = position;
 			point.localScale = scale;
 			point.SetParent(transform, false);
+			points[i] = point;
 		}
 	}
+
+    void Update()
+    {
+		float time = Time.time;
+
+		for (int i = 0; i < points.Length; i++)
+        {
+			Transform point = points[i];
+			Vector3 position = point.localPosition;
+			position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }    
+    }
 }
--- Assets/Shaders/Point Shader.shader
+++ Assets/Shaders/Point Shader.shader
@@ -17,7 +17,7 @@
 		float _Smoothness;
 
 		void ConfigureSurface (Input input, inout SurfaceOutputStandard surface) {
-			surface.Albedo.rg = input.worldPos.xy * 0.5 + 0.5;
+			surface.Albedo.rg = saturate(input.worldPos.xy * 0.5 + 0.5);
 			surface.Smoothness = _Smoothness;
         }
 
