1b13610687fbb4af6ec49185cb439a38d454a36e
Andrej <glazba@gmail.com>
2020-10-03 21:41:06 +0200

    my first shader ever

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform pointPrefab = default;
+
+	[SerializeField, Range (10, 100)]
+	int resolution = 10;
+
+	private Transform point = default;
+
+    void Awake()
+    {
+		float step = 2f / resolution;
+        var position = Vector3.zero;
+		var scale = Vector3.one * step;
+		for (int i = 0; i < resolution; i++)
+		{
+			Transform point = Instantiate(pointPrefab);
+			position.x = (i + 0.5f) * step - 1f;
+			position.y = position.x * position.x * position.x;
+			point.localPosition = position;
+			point.localScale = scale;
+			point.SetParent(transform, false);
+		}
+	}
+}
