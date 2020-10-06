baeb060eaed8ce717abe7fec495295626348058a
Andrej <glazba@gmail.com>
2020-10-06 21:30:27 +0200

    fixed Jumping (apply force before adding it to rigidbody)

--- Assets/Scripts/01_Movement/MovingSphere_Physics.cs
+++ Assets/Scripts/01_Movement/MovingSphere_Physics.cs
@@ -3,29 +3,18 @@
 public class MovingSphere_Physics : MonoBehaviour
 {
 
-	[SerializeField, Range(0f, 100f)]
-	float maxSpeed = 10f;
-
-	[SerializeField, Range(0f, 100f)]
-	float maxAcceleration = 10f;
-
-	[SerializeField, Range(0f, 1f)]
-	float bounciness = 0.5f;
-
-	[SerializeField]
-	Rect allowedArea = new Rect(-4.5f, -4.5f, 9f, 9f);
-
+	[SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
+	[SerializeField, Range(0f, 100f)] float maxAcceleration = 10f;
+	[SerializeField, Range(0F, 10F)] float jumpHeight = 2f;
 	Vector3 velocity, desiredVelocity;
-
 	Rigidbody body;
+	bool desiredJump;
 
-    void Awake ()
-    {
+    void Awake () {
 		body = GetComponent<Rigidbody>();
     }
 
-	void Update()
-	{
+	void Update() {
 		Vector2 playerInput;
 		playerInput.x = Input.GetAxis("Horizontal");
 		playerInput.y = Input.GetAxis("Vertical");
@@ -33,11 +22,10 @@ public class MovingSphere_Physics : MonoBehaviour
 
 		desiredVelocity =
 			new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
-
+		desiredJump |= Input.GetButtonDown("Jump");
 	}
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
 		velocity = body.velocity;
 		float maxSpeedChange = maxAcceleration * Time.deltaTime;
 		velocity.x =
@@ -45,6 +33,15 @@ public class MovingSphere_Physics : MonoBehaviour
 		velocity.z =
 			Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
 
+        if (desiredJump) {
+			Jump();
+			desiredJump = false;
+        }
+
 		body.velocity = velocity;
 	}
+
+    void Jump(){
+		velocity.y += Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+    }
 }
