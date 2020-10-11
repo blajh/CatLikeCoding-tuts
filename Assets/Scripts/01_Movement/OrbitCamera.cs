8747601e93ee098238635a5c02d780d74664a41a
blajh <glazba@gmail.com>
2020-10-11 22:59:56 +0200

    Maintaining Relative Position

--- Assets/Scripts/01_Movement/OrbitCamera.cs
+++ Assets/Scripts/01_Movement/OrbitCamera.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
-public class OrbitCamera : MonoBehaviour
-{
+public class OrbitCamera : MonoBehaviour {
 
+	[SerializeField] Transform focus = default;
+	[SerializeField, Range(1f, 20f)] float distance = 5f;
+
+	void LateUpdate() {
+		Vector3 focusPoint = focus.position;
+		Vector3 lookDirection = transform.forward;
+		transform.localPosition = focusPoint - lookDirection * distance;
+	}
 }
