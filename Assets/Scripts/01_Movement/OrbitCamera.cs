32a44fe6db7ed813952839ca4ed81d04bd022a5c
blajh <glazba@gmail.com>
2020-10-11 23:17:31 +0200

    Centering the Focus

--- Assets/Scripts/01_Movement/OrbitCamera.cs
+++ Assets/Scripts/01_Movement/OrbitCamera.cs
@@ -6,6 +6,7 @@ public class OrbitCamera : MonoBehaviour {
 	[SerializeField] Transform focus = default;
 	[SerializeField, Range(1f, 20f)] float distance = 5f;
 	[SerializeField, Min(0f)] float focusRadius = 1f;
+	[SerializeField, Range(0f, 1f)] float focusCentering = 0.75f;
 
 	Vector3 focusPoint;
 
@@ -23,10 +24,14 @@ public class OrbitCamera : MonoBehaviour {
 		Vector3 targetPoint = focus.position;
 		if (focusRadius > 0f) {
 			float distance = Vector3.Distance(targetPoint, focusPoint);
-			if (distance > focusRadius) {
-				focusPoint = Vector3.Lerp(
-					targetPoint, focusPoint, focusRadius / distance);
+			float t = 1f;
+			if (distance > 0.01f && focusCentering > 0f) {
+				t = Mathf.Pow(1f - focusCentering, Time.unscaledDeltaTime);
 			}
+			if (distance > focusRadius) {
+				t = Mathf.Min(t, focusRadius / distance);
+			}
+			focusPoint = Vector3.Lerp(targetPoint, focusPoint, t);
 		}
 		else {
 			focusPoint = targetPoint;
