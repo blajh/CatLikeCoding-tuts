4678e6b14b8133fa71941ff617f7349818d2fbea
blajh <glazba@gmail.com>
2020-10-14 22:37:10 +0200

    Best and Worst

--- Assets/Scripts/00_Basics/FrameRateCounter.cs
+++ Assets/Scripts/00_Basics/FrameRateCounter.cs
@@ -9,17 +9,31 @@ public class FrameRateCounter : MonoBehaviour
 	[SerializeField, Range(0.1f, 2f)] float sampleDuration = .25f;
 
 	int frames;
-	float duration;
+	float duration, worstDuration, bestDuration = float.MaxValue;
 
 	void Update() {
 		float frameDuration = Time.unscaledDeltaTime;
 		frames += 1;
 		duration += frameDuration;
 
+		if (frameDuration < bestDuration) {
+			bestDuration = frameDuration;
+		}
+
+		if (frameDuration > worstDuration) {
+			worstDuration = frameDuration;
+		}
+
 		if (duration >= sampleDuration) {
-			display.SetText("FPS\n{0:0}\n000\n000", frames / duration);
+			display.SetText(
+				"FPS\nB:{0:0}\nA:{1:0}\nW:{2:0}",
+				1f / bestDuration,
+				frames / duration,
+				1f / worstDuration);
 			frames = 0;
 			duration = 0f;
+			bestDuration = float.MaxValue;
+			worstDuration = 0f;
 		}
 	}
 }
