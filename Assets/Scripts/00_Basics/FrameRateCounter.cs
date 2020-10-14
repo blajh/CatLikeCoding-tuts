3c0b6cf571b55fee5413ddbed7d48de2a87f0586
blajh <glazba@gmail.com>
2020-10-14 22:26:06 +0200

    Updating the Display

---
+++ Assets/Scripts/00_Basics/FrameRateCounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FrameRateCounter : MonoBehaviour
+{
+	[SerializeField]
+	TextMeshProUGUI display = default;
+
+	private void Update() {
+		float frameDuration = Time.unscaledDeltaTime;
+		display.SetText("FPS\n{0:0}\n000\n000", 1f / frameDuration);
+	}
+}
