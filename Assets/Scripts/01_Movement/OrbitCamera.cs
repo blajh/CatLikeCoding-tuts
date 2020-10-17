2a622c06243846a0772b45f08d452e5b5bdbc447
blajh <glazba@gmail.com>
2020-10-17 23:13:49 +0200

    Reducing Look Distance

--- Assets/Scripts/01_Movement/OrbitCamera.cs
+++ Assets/Scripts/01_Movement/OrbitCamera.cs
@@ -34,6 +34,13 @@ public class OrbitCamera : MonoBehaviour {
 		}
 		Vector3 lookDirection = lookRotation * Vector3.forward;
 		Vector3 lookPosition = focusPoint - lookDirection * distance;
+
+		if (Physics.Raycast(
+			focusPoint, -lookDirection, out RaycastHit hit, distance
+		)) {
+			lookPosition = focusPoint - lookDirection * hit.distance;
+		}
+
 		transform.SetPositionAndRotation(lookPosition, lookRotation);
 	}
 
