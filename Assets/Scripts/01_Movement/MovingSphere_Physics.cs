8eef245b80c7198fc233c10758326172cc8b539c
blajh <glazba@gmail.com>
2020-10-17 23:09:48 +0200

    Normalized Direction

--- Assets/Scripts/01_Movement/MovingSphere_Physics.cs
+++ Assets/Scripts/01_Movement/MovingSphere_Physics.cs
@@ -35,8 +35,14 @@ public class MovingSphere_Physics : MonoBehaviour {
 		playerInput = Vector2.ClampMagnitude(playerInput, 1f);
 
 		if (playerInputSpace) {
-			desiredVelocity = playerInputSpace.TransformDirection(
-				playerInput.x, 0f, playerInput.y) * maxSpeed;
+			Vector3 forward = playerInputSpace.forward;
+			forward.y = 0f;
+			forward.Normalize();
+			Vector3 right = playerInputSpace.right;
+			right.y = 0f;
+			right.Normalize();
+			desiredVelocity =
+				(forward * playerInput.y + right * playerInput.x) * maxSpeed;
 		}
 		else {
 		desiredVelocity =
